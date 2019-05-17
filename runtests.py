716cbc8c586fe37d616dc9e8746d14f1d6c659c5
Matt Westcott <matt@west.co.tt>
2019-05-17 19:04:17 +0100

    Add test framework

---
+++ runtests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+from django.core.management import execute_from_command_line
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+execute_from_command_line([sys.argv[0], 'test'])
