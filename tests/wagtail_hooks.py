c3cbfe08b4503c5bac94c74395c211533ee57320
Matt Westcott <matt@west.co.tt>
2019-05-20 12:20:09 +0100

    Use viewset for API page chooser

--- tests/wagtail_hooks.py
+++ tests/wagtail_hooks.py
@@ -1,19 +1,6 @@
-from django.conf.urls import include, url
 from wagtail.core import hooks
 
 from tests import views
-
-
-@hooks.register('register_admin_urls')
-def register_admin_urls():
-    api_page_chooser_urls = [
-        url(r'^chooser/$', views.ChoosePageAPIView.as_view(), name='choose'),
-        url(r'^chooser/(\d+)/$', views.ChosenPageAPIView.as_view(), name='chosen'),
-    ]
-
-    return [
-        url(r'^api-page-chooser/', include((api_page_chooser_urls, 'api_page_chooser'), namespace='api_page_chooser')),
-    ]
 
 
 @hooks.register('register_admin_viewset')
@@ -24,3 +11,8 @@ def register_site_chooser_viewset():
 @hooks.register('register_admin_viewset')
 def register_page_chooser_viewset():
     return views.PageChooserViewSet('page_chooser', url_prefix='page-chooser')
+
+
+@hooks.register('register_admin_viewset')
+def register_api_page_chooser_viewset():
+    return views.APIPageChooserViewSet('api_page_chooser', url_prefix='api-page-chooser')
