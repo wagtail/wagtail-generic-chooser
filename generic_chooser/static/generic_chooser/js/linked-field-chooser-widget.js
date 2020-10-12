79fcb64296a019528a6f4a4049c48c7edc790b50
Matt Westcott <matt@west.co.tt>
2020-10-12 17:03:57 +0100

    Extend LinkedFieldChooserWidget with extra lookup types

--- generic_chooser/static/generic_chooser/js/linked-field-chooser-widget.js
+++ generic_chooser/static/generic_chooser/js/linked-field-chooser-widget.js
@@ -2,10 +2,17 @@ function LinkedFieldChooserWidget(id, opts) {
     /* A ChooserWidget which can pull values from other form elements to pass
     to the chooser modal as extra URL parameters. Define these by passing
     a 'linkedFields' dictionary in opts, of the form:
-        {'urlParamName': '#some-css-selector'}
-    which means: when generating the modal URL, look up the element matching
-    some-css-selector, get its value, and add that to the URL as the
+        {'urlParamName': {'id': 'some-element-id'}}
+    which means: when generating the modal URL, look up the element with
+    id="some-element-id", get its value, and add that to the URL as the
     parameter urlParamName.
+
+    Alternative lookup types:
+        {'urlParamName': {'selector': '.some-selector'}}
+        - look up the element matching the given CSS selector
+        {'urlParamName': {'match': r'^body-\d+-value-', 'append': 'country'}}
+        - look up the ID formed by matching the CURRENT widget's ID against
+          the given regexp, then appending the given string
     */
     opts = opts || {};
     ChooserWidget.call(this, id, opts);
@@ -20,9 +27,27 @@ LinkedFieldChooserWidget.prototype.getModalURL = function() {
 
     queryParams = [];
     for (var param in this.linkedFields) {
-        var selector = this.linkedFields[param];
-        var val = $(document).find(selector).val();
-        queryParams.push({'name': param, 'value': val});
+        var lookup = this.linkedFields[param];
+        var val;
+        if (typeof(lookup) == 'string') {
+            val = $(document).find(lookup).val();
+        } else if (lookup.id) {
+            val = $(document).find('#' + lookup.id).val();
+        } else if (lookup.selector) {
+            val = $(document).find(lookup.selector).val();
+        } else if (lookup.match) {
+            var match = this.id.match(new RegExp(lookup.match));
+            if (match) {
+                var id = match[0];
+                if (lookup.append) {
+                    id += lookup.append;
+                }
+                val = $(document).find('#' + id).val();
+            }
+        }
+        if (val) {
+            queryParams.push({'name': param, 'value': val});
+        }
     }
     if (url.indexOf('?') == -1) {
         url += '?' + $.param(queryParams);
