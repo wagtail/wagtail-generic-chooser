8ccfceb992632661793e5ed558d1004976aaa1af
Matt Westcott <matt@west.co.tt>
2021-05-27 18:31:21 +0100

    Refactor out the 'widget' object into an extensible class

--- generic_chooser/static/generic_chooser/js/chooser-widget.js
+++ generic_chooser/static/generic_chooser/js/chooser-widget.js
@@ -1,4 +1,4 @@
-function createChooserWidget(id, opts) {
+function ChooserWidget(id, opts) {
     /*
     id = the ID of the HTML element where chooser behaviour should be attached
     opts = dictionary of configuration options, which may include:
@@ -7,70 +7,78 @@ function createChooserWidget(id, opts) {
     */
 
     opts = opts || {};
+    var self = this;
 
-    var chooserElement = $('#' + id + '-chooser');
-    var docTitle = chooserElement.find('.title');
-    var input = $('#' + id);
-    var editLink = chooserElement.find('.edit-link');
-    var editLinkWrapper = chooserElement.find('.edit-link-wrapper');
-    if (!editLink.attr('href')) {
-        editLinkWrapper.hide();
+    this.chooserElement = $('#' + id + '-chooser');
+    this.titleElement = this.chooserElement.find('.title');
+    this.inputElement = $('#' + id);
+    this.editLinkElement = this.chooserElement.find('.edit-link');
+    this.editLinkWrapper = this.chooserElement.find('.edit-link-wrapper');
+    if (!this.editLinkElement.attr('href')) {
+        this.editLinkWrapper.hide();
     }
-    var chooseButton = $('.action-choose', chooserElement);
+    this.chooseButton = $('.action-choose', this.chooserElement);
+    this.idForLabel = null;
 
-    var widget = {
-        idForLabel: null,
-        getState: function() {
-            return {
-                'value': input.val(),
-                'title': docTitle.text(),
-                'edit_item_url': editLink.attr('href')
-            };
-        },
-        getValue: function() {
-            return input.val();
-        },
-        setState: function(newState) {
-            if (newState && newState.value !== null && newState.value !== '') {
-                input.val(newState.value);
-                docTitle.text(newState.title);
-                chooserElement.removeClass('blank');
-                if (newState.edit_item_url) {
-                    editLink.attr('href', newState.edit_item_url);
-                    editLinkWrapper.show();
-                } else {
-                    editLinkWrapper.hide();
-                }
-            } else {
-                input.val('');
-                chooserElement.addClass('blank');
-            }
-        },
-        focus: function() {
-            chooseButton.focus();
-        }
+    this.modalResponses = {};
+    this.modalResponses[opts.modalWorkflowResponseName || 'chosen'] = function(data) {
+        self.setState({
+            'value': data.id,
+            'title': data.string,
+            'edit_item_url': data.edit_link
+        });
     };
 
-    chooseButton.on('click', function() {
-        var responses = {};
-        responses[opts.modalWorkflowResponseName || 'chosen'] = function(snippetData) {
-            widget.setState({
-                'value': snippetData.id,
-                'title': snippetData.string,
-                'edit_item_url': snippetData.edit_link
-            });
-        };
-
-        ModalWorkflow({
-            url: chooserElement.data('choose-modal-url'),
-            onload: GENERIC_CHOOSER_MODAL_ONLOAD_HANDLERS,
-            responses: responses
-        });
+    this.chooseButton.on('click', function() {
+        self.openModal();
     });
 
-    $('.action-clear', chooserElement).on('click', function() {
-        widget.setState(null);
+    $('.action-clear', this.chooserElement).on('click', function() {
+        self.setState(null);
     });
+}
 
-    return widget;
+ChooserWidget.prototype.getModalURL = function() {
+    return this.chooserElement.data('choose-modal-url');
+};
+
+ChooserWidget.prototype.openModal = function() {
+    ModalWorkflow({
+        url: this.getModalURL(),
+        onload: GENERIC_CHOOSER_MODAL_ONLOAD_HANDLERS,
+        responses: this.modalResponses
+    });
+};
+
+ChooserWidget.prototype.setState = function(newState) {
+    if (newState && newState.value !== null && newState.value !== '') {
+        this.inputElement.val(newState.value);
+        this.titleElement.text(newState.title);
+        this.chooserElement.removeClass('blank');
+        if (newState.edit_item_url) {
+            this.editLinkElement.attr('href', newState.edit_item_url);
+            this.editLinkWrapper.show();
+        } else {
+            this.editLinkWrapper.hide();
+        }
+    } else {
+        this.inputElement.val('');
+        this.chooserElement.addClass('blank');
+    }
+};
+
+ChooserWidget.prototype.getState = function() {
+    return {
+        'value': this.inputElement.val(),
+        'title': this.titleElement.text(),
+        'edit_item_url': this.editLinkElement.attr('href')
+    };
+};
+
+ChooserWidget.prototype.getValue = function() {
+    return this.inputElement.val();
+};
+
+ChooserWidget.prototype.focus = function() {
+    this.chooseButton.focus();
 }
